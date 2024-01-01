088507bb6f97e8847f342de8d31e2877a697a1a0
luong2k3dev <tunhatluong2k3@gmail.com>
2024-01-01 16:41:02 +0700

    Update chart

--- src/app/manage/chart-order/page.jsx
+++ src/app/manage/chart-order/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { Tabs } from 'antd';
-import ChartByDepartment from './chartByDepartment';
+import Chart from './chart';
 const { TabPane } = Tabs;
 import BreadCrumb from '../../../components/Manage/Breadcrumb';
 import { getListDepartment } from '../services/department.service';
@@ -22,13 +22,13 @@ export default function ChartOrder() {
       <BreadCrumb link="http://localhost:3000/manage" title_1="Quản lý đơn khám" title_2="Thống kê đơn khám" />
       <Tabs defaultActiveKey="1" onChange={(key) => console.log(key)}>
         <TabPane tab="Theo chuyên khoa" key="1">
-          <ChartByDepartment tab="1" departments={departments} />
+          <Chart tab="1" departments={departments} />
         </TabPane>
         <TabPane tab="Theo bác sĩ" key="2">
-          <ChartByDepartment tab="2" departments={departments} />
+          <Chart tab="2" departments={departments} />
         </TabPane>
         <TabPane tab="Theo độ tuổi" key="3">
-          <ChartByDepartment tab="3" departments={departments} />
+          <Chart tab="3" departments={departments} />
         </TabPane>
       </Tabs>
     </div>
